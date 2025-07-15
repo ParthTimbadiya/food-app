ada194102fa287b3a48565bb5c29f9c19dbf10f7
Parth Timbadiya <parthtimbadiya21@gmail.com>
2025-07-15 12:27:52 +0530

    update user

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,59 @@
+const userModel = require("../models/userModel");
 const getUserController = async (req, res) => {
-  res.status(200).send("user data");
+  try {
+    const user = await userModel.findById({ _id: req.user.id });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "user not found",
+      });
+    }
+    user.password = undefined;
+    res.status(200).send({
+      success: true,
+      message: "user get successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "error in get user api",
+      error,
+    });
+  }
 };
 
-module.exports = { getUserController };
+// update controller
+
+const updateUserController = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.user.id);
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "user not found",
+      });
+    }
+    const { userName, address, phone } = req.body;
+    if (userName) user.userName = userName;
+    if (address) user.address = address;
+    if (phone) user.phone = phone;
+
+    // save user
+    await user.save();
+    res.status(200).send({
+      success: true,
+      message: "user update successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "error in update user api",
+      error,
+    });
+  }
+};
+
+module.exports = { getUserController, updateUserController };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { getUserController } = require("../controller/userController");
+const {
+  getUserController,
+  updateUserController,
+} = require("../controller/userController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const routes = express.Router();
 
 routes.get("/getUser", authMiddleware, getUserController);
+
+routes.put("/updateUser", authMiddleware, updateUserController);
 
 module.exports = routes;
